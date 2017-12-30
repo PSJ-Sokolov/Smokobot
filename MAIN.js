3f023bba562dd1b8241108c7b1aca509c907c172
Philip SJ Sokolov <PhilipSokolov@protonmail.com>
2017-12-30 11:28:52 +0100

    Net niet de eerste commit

---
+++ MAIN.js
@@ -0,0 +1,42 @@
+//Configureer de dependencies.
+var Discord = require('discord.io');
+var logger  = require('winston');
+var auth    = require('./auth.json');
+
+//Configureer de logger.
+logger.remove(logger.transports.Console);
+logger.add(logger.transports.Console,{
+    colorize: true
+});
+logger.level = 'debug';
+
+//Initialize the Smokobot.
+var Smokobot = new Discord.Client({token: auth.token,autorun: true});
+
+//Verbind.
+Smokobot.on('ready', function (evt) {
+    logger.info('Verbinding tot stand gebracht!');
+    logger.info('Ingelogd als:');
+    logger.info(Smokobot.username + ' - (' + Smokobot.id + ')');
+});
+
+Smokobot.on('message', function (user, userID, channelID, message, evt) {
+    //Luister naar berichten die starten met / om te interpreteren als commando's.
+    if (message.substring(0, 1) == '/') {
+        var lijst_MetArgumenten = message.substring(1).split(' ');
+        var commando = lijst_MetArgumenten[0];
+
+        lijst_MetArgumenten = lijst_MetArgumenten.splice(1);
+
+        switch(commando) {
+            // !ping
+        case 'ping':
+            Smokobot.sendMessage({ to: channelID, message: 'Pong!' });
+            break;
+        case 'git':
+            Smokobot.sendMessage({ to: channelID, message: 'Ik wordt gehost op:'https://github.com/PSJ-Sokolov/Smokobot.git });
+        default:
+            Smokobot.sendMessage({ to: channelID, message: 'Unknown command.' });
+        }
+    }
+})
